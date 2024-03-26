26a2d4f9ed26d63936af59e2106e87a42a0cbe9a
fnx3 <154522683+fnx3@users.noreply.github.com>
2024-03-26 20:51:03 +0300

    Update UserService.java

--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -6,7 +6,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public interface UserService {
-    UserDaoJDBCImpl userDaoJDBSImpl = new UserDaoJDBCImpl();
+    
     void createUsersTable();
 
     void dropUsersTable();
